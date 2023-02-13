649a8bc337435cc206aeb924b1e1d60ace01da0d
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:40 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 3D LiDAR Topography Visualization
 
-![3D LiDAR Topography Visualization](LidarLand.png)
+![3D LiDAR Topography Visualization](LidarLand-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -22,10 +22,11 @@ The demo can be used as an example or a seed project. Local execution requires t
 This example shows visualization of a LiDAR scanned 3D object with LightningChart JS 3D Charts
 
 This is LiDAR data of Badwater Basin in Death Valley National Park, Inyo County, California with 1.139.662 points
- 
+
 LightningChart JS is suitable for really fast visualization of LiDAR data sets in range of tens of millions samples, with 3D points or 3D surface. This example shows the approach with points. In this example, each point is coloured dynamically by their Y coordinate according to a value -> color lookup table.
 
-Lidar data from: [Philipp Urech](https://sketchfab.com/3d-models/badwater-basin-30-sections-across-a-wash-ca8eebf6798d4b0a967336e16f066754) - Licensed under Creative Commons Attribute. 
+Lidar data from: [Philipp Urech](https://sketchfab.com/3d-models/badwater-basin-30-sections-across-a-wash-ca8eebf6798d4b0a967336e16f066754) - Licensed under Creative Commons Attribute.
+
 
 ## API Links
 
@@ -41,27 +42,27 @@ Lidar data from: [Philipp Urech](https://sketchfab.com/3d-models/badwater-basin-
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chart3d.html
-[3D axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis3d.html
-[3D Point Cloud Series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/pointcloudseries3d.html
-[3D Point Series Types]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#pointseriestypes3d
-[3D Point Style]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#pointstyle3d
-[Paletted Fill]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/palettedfill.html
+[3D chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart3D.html
+[3D axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis3D.html
+[3D Point Cloud Series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PointCloudSeries3D.html
+[3D Point Series Types]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/PointSeriesTypes3D-1.html
+[3D Point Style]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/PointStyle3D.html
+[Paletted Fill]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PalettedFill.html
 
--- src/index.js
+++ src/index.js
@@ -5,32 +5,41 @@
 const lcjs = require('@arction/lcjs')
 
 // Extract required parts from LightningChartJS.
-const {
-    lightningChart,
-    PointSeriesTypes3D,
-    PointStyle3D,
-    ColorRGBA,
-    PalettedFill,
-    LUT,
-    AxisTickStrategies,
-    emptyLine,
-    emptyFill,
-    Themes,
-} = lcjs
-
+const { lightningChart, PointSeriesTypes3D, PointStyle3D, ColorRGBA, PalettedFill, LUT, AxisTickStrategies, emptyLine, emptyFill, Themes } =
+    lcjs
 
 // Create 3D chart
-const chart = lightningChart().Chart3D({
-    disableAnimations: true,
-    // theme: Themes.darkGold
-})
+const chart = lightningChart()
+    .Chart3D({
+        // theme: Themes.darkGold
+    })
     .setTitle(`LiDAR Point Cloud`)
-    .setSeriesBackgroundFillStyle(emptyFill)
 
-chart.getDefaultAxisX().setTickStrategy(AxisTickStrategies.Empty).setStrokeStyle(emptyLine)
-chart.getDefaultAxisY().setTickStrategy(AxisTickStrategies.Empty).setStrokeStyle(emptyLine).setInterval(-111, 2500, false, true)
-chart.getDefaultAxisZ().setTickStrategy(AxisTickStrategies.Empty).setStrokeStyle(emptyLine)
-chart.setCameraLocation({ x: -1.5, y: .5, z: .8 })
+chart
+    .getDefaultAxisX()
+    .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
+        ticks
+            .setMajorTickStyle((major) => major.setLabelFillStyle(emptyFill))
+            .setMinorTickStyle((minor) => minor.setLabelFillStyle(emptyFill)),
+    )
+    .setStrokeStyle(emptyLine)
+chart
+    .getDefaultAxisY()
+    .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
+        ticks
+            .setMajorTickStyle((major) => major.setLabelFillStyle(emptyFill))
+            .setMinorTickStyle((minor) => minor.setLabelFillStyle(emptyFill)),
+    )
+    .setStrokeStyle(emptyLine)
+    .setInterval({ start: -111, end: 2500 })
+chart
+    .getDefaultAxisZ()
+    .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
+        ticks
+            .setMajorTickStyle((major) => major.setLabelFillStyle(emptyFill))
+            .setMinorTickStyle((minor) => minor.setLabelFillStyle(emptyFill)),
+    )
+    .setStrokeStyle(emptyLine)
 
 const legend = chart.addLegendBox()
 
@@ -38,7 +47,7 @@ let totalPointsCount = 0
 
 const loadBinaryLidarFile = async (assetName) => {
     // Load LiDAR data as custom formatted binary file (contains total number of data points + each point X, Y, Z values)
-    const result = await fetch( document.head.baseURI + `examples/assets/0911/${assetName}`)
+    const result = await fetch(document.head.baseURI + `examples/assets/0911/${assetName}`)
     const blob = await result.blob()
     const arrayBuffer = await blob.arrayBuffer()
     // Read number of points as first Uint32 value.
@@ -46,28 +55,28 @@ const loadBinaryLidarFile = async (assetName) => {
     const pointsCount = new Uint32Array(arrayBuffer.slice(arrayBufferBytePos, arrayBufferBytePos + 4))[0]
     arrayBufferBytePos += 4
     // Read binary data into XYZRGB points.
-    const dataPoints = new Array(pointsCount).fill(0).map(_ => ({}))
+    const dataPoints = new Array(pointsCount).fill(0).map((_) => ({}))
     // X values in order.
     const xValuesByteLength = pointsCount * 2
     const xValues = new Int16Array(arrayBuffer.slice(arrayBufferBytePos, arrayBufferBytePos + xValuesByteLength))
     arrayBufferBytePos += xValuesByteLength
-    xValues.forEach((x, i) => dataPoints[i].x = x)
+    xValues.forEach((x, i) => (dataPoints[i].x = x))
     // Y values in order.
     const yValuesByteLength = pointsCount * 2
     const yValues = new Int16Array(arrayBuffer.slice(arrayBufferBytePos, arrayBufferBytePos + yValuesByteLength))
     arrayBufferBytePos += yValuesByteLength
-    yValues.forEach((y, i) => dataPoints[i].y = y)
+    yValues.forEach((y, i) => (dataPoints[i].y = y))
     // Z values in order.
-    const zValuesByteLength = pointsCount * 2 
+    const zValuesByteLength = pointsCount * 2
     const zValues = new Int16Array(arrayBuffer.slice(arrayBufferBytePos, arrayBufferBytePos + zValuesByteLength))
     arrayBufferBytePos += zValuesByteLength
-    zValues.forEach((z, i) => dataPoints[i].z = z)
+    zValues.forEach((z, i) => (dataPoints[i].z = z))
 
     // Add Point Series with lidar point cloud data.
     const series = chart
         .addPointSeries({
-            type: PointSeriesTypes3D.Pixelated,
-            individualPointColorEnabled: true,
+            type: PointSeriesTypes3D.Pixelated,
+            individualPointColorEnabled: true,
         })
         .add(dataPoints)
         .setPointStyle(
@@ -84,7 +93,7 @@ const loadBinaryLidarFile = async (assetName) => {
                             { value: 400, color: ColorRGBA(202, 22, 80) },
                             { value: 600, color: ColorRGBA(148, 18, 18) },
                             { value: 800, color: ColorRGBA(178, 0, 255) },
-                            { value: 1000, color: ColorRGBA(225, 122, 239) }
+                            { value: 1000, color: ColorRGBA(225, 122, 239) },
                         ],
                     }),
                 }),
@@ -100,8 +109,6 @@ const loadBinaryLidarFile = async (assetName) => {
     return series
 }
 
-loadBinaryLidarFile('NEWLAND.bin')
-    .then(series => {
-        series.setName('Badwater Basin')
-    })
-
+loadBinaryLidarFile('NEWLAND.bin').then((series) => {
+    series.setName('Badwater Basin')
+})
